07c8d4e7b411159ca642aa64729204a6db21fae1
Alejandro20042 <alejandrosillasalcantar.com@gmail.com>
2024-02-29 13:48:23 -0700

    Actualizacion

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LoginComponent
+  ],
+  providers: [],
+  bootstrap: [],
+})
+export class AppModule { }
